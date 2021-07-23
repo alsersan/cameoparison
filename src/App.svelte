81cc88f1f221b5d2af5e35b4f7537a04cacd697a
alsersan <alsersan20@gmail.com>
2021-07-24 00:54:46 +0200

    Add functionality to buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,43 @@
 <script>
+  import { onMount } from 'svelte';
+  import { select } from './select';
   import Welcome from './screens/Welcome.svelte';
-  let state = 'welcome';
 
-  const start = (e) => {
-    console.log(e);
+  let state = 'welcome'; // or 'playing'
+  let celebsPromise;
+  let selection;
+
+  const start = async (e) => {
+    const { celebs, lookup } = await celebsPromise;
+    selection = select(celebs, lookup, e.detail.category.slug);
+    state = 'playing';
+    console.log(selection);
   };
+
+  const loadCelebs = async () => {
+    const res = await fetch('https://cameo-explorer.netlify.app/celebs.json');
+    const data = await res.json();
+
+    const lookup = new Map();
+    data.forEach((c) => lookup.set(c.id, c));
+
+    const subset = new Set();
+    data.forEach((c) => {
+      if (c.reviews >= 50) {
+        subset.add(c);
+        c.similar.forEach((id) => subset.add(lookup.get(id)));
+      }
+    });
+
+    return {
+      celebs: Array.from(subset),
+      lookup,
+    };
+  };
+
+  onMount(() => {
+    celebsPromise = loadCelebs();
+  });
 </script>
 
 <main>
