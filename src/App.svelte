e99e3b64fb2c61587114d7530f21e2ef03d29398
alsersan <alsersan20@gmail.com>
2021-07-23 23:31:35 +0200

    Add buttons to Welcome screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script>
   import Welcome from './screens/Welcome.svelte';
   let state = 'welcome';
+
+  const start = (e) => {
+    console.log(e);
+  };
 </script>
 
 <main>
   {#if state === 'welcome'}
-    <Welcome />
+    <Welcome on:select={start} />
   {:else if state === 'playing'}
     <p>Playing content here</p>
   {/if}
@@ -21,13 +25,6 @@
     flex-direction: column;
     justify-content: center;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
   }
 
   @media (min-width: 640px) {
